d37b4b4d7619ebe761c4aa82f0e385018841d100
Goel Shubham (RBEI/EAA2) <hgs9kor@bosch.com>
2020-10-24 18:15:36 +0530

    Updates

---
+++ precache-manifest.03fa09c5763a429bad56259d98376ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11165aa3cee74e4819750c33defe4c5",
+    "url": "/PokeGame/index.html"
+  },
+  {
+    "revision": "c20f46203aad4bc2ad5b",
+    "url": "/PokeGame/static/css/main.a3149aa5.chunk.css"
+  },
+  {
+    "revision": "b02355be7d3d60363e22",
+    "url": "/PokeGame/static/js/2.592fc630.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/PokeGame/static/js/2.592fc630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c20f46203aad4bc2ad5b",
+    "url": "/PokeGame/static/js/main.c285c9d6.chunk.js"
+  },
+  {
+    "revision": "0923a65571871a98c854",
+    "url": "/PokeGame/static/js/runtime-main.bfd26354.js"
+  }
+]);
